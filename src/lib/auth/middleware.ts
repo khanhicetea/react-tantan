985bdab7a58b37ad114e9264dfa29dd60c89fe87
Nathaniel Tampus <nate@mugnavo.com>
2025-09-24 15:33:19 +0800

    chore: remove type for middleware

--- src/lib/auth/middleware.ts
+++ src/lib/auth/middleware.ts
@@ -3,27 +3,25 @@ import { getRequest, setResponseStatus } from "@tanstack/react-start/server";
 import { auth } from "~/lib/auth/auth";
 
 // https://tanstack.com/start/latest/docs/framework/react/middleware
-// This is a sample middleware that you can use in your server functions.
+// This is just an example middleware that you can modify and use in your server functions or routes.
 
 /**
- * Middleware to force authentication on a server function, and add the user to the context.
+ * Middleware to force authentication on server requests (including server functions), and add the user to the context.
  */
-export const authMiddleware = createMiddleware({ type: "function" }).server(
-  async ({ next }) => {
-    const session = await auth.api.getSession({
-      headers: getRequest().headers,
-      query: {
-        // ensure session is fresh
-        // https://www.better-auth.com/docs/concepts/session-management#session-caching
-        disableCookieCache: true,
-      },
-    });
+export const authMiddleware = createMiddleware().server(async ({ next }) => {
+  const session = await auth.api.getSession({
+    headers: getRequest().headers,
+    query: {
+      // ensure session is fresh
+      // https://www.better-auth.com/docs/concepts/session-management#session-caching
+      disableCookieCache: true,
+    },
+  });
 
-    if (!session) {
-      setResponseStatus(401);
-      throw new Error("Unauthorized");
-    }
+  if (!session) {
+    setResponseStatus(401);
+    throw new Error("Unauthorized");
+  }
 
-    return next({ context: { user: session.user } });
-  },
-);
+  return next({ context: { user: session.user } });
+});
