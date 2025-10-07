dc681fa082f1116beccd8cbfc03dfb35503085ba
KhanhIceTea <khanhicetea@gmail.com>
2025-10-07 23:19:49 +0700

    currentHost temp fix

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,13 +1,12 @@
 import { createEnv } from "@t3-oss/env-core";
 import * as z from "zod";
-import { config } from "dotenv";
 
-config();
+const currentHost = typeof window !== "undefined" ? `${window.location.protocol}://${window.location.host}` : "http://localhost:3000";
 
 export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
-    VITE_BASE_URL: z.url().default("http://localhost:3000"),
+    VITE_BASE_URL: z.url().default(currentHost),
   },
   runtimeEnv: import.meta.env,
 });
