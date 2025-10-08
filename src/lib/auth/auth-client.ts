539ba55b261c97857194220ebbde60e1b93b912a
KhanhIceTea <khanhicetea@gmail.com>
2025-10-08 10:04:25 +0700

    test build

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 import { env } from "~/env/client";
 
-console.log({viteBaseUrl: env.VITE_BASE_URL})
+console.log({baseURL: env.VITE_BASE_URL})
 
 const authClient = createAuthClient({
   baseURL: env.VITE_BASE_URL,
