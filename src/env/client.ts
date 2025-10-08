005ab532699d28bac22e8284372c2af8e76c65c3
KhanhIceTea <khanhicetea@gmail.com>
2025-10-08 08:58:07 +0700

    test new vite env fix bug

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,12 +1,12 @@
 import { createEnv } from "@t3-oss/env-core";
 import * as z from "zod";
 
-const currentHost = typeof window !== "undefined" ? `${window.location.protocol}://${window.location.host}` : "http://localhost:3000";
+// const currentHost = typeof window !== "undefined" ? `${window.location.protocol}://${window.location.host}` : "http://localhost:3000";
 
 export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
-    VITE_BASE_URL: z.url().default(currentHost),
+    VITE_BASE_URL: z.url(),
   },
   runtimeEnv: import.meta.env,
 });
--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 import { env } from "~/env/client";
 
+console.log({viteBaseUrl: env.VITE_BASE_URL})
+
 const authClient = createAuthClient({
   baseURL: env.VITE_BASE_URL,
 });
